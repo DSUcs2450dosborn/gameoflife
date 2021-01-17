5a6ef9f6925be5942613f7c13d84ab4e20773184
Doug Osborn <d00303905@dmail.dixie.edu>
2021-01-17 12:28:03 -0700

    added end game score and logo  with Shake to reset

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+// compare array
+function isDead () {
+    // debugger;
+    return state.every((val, index) => val === deadstate[index])
+}
 // Core function
 function gameOfLife () {
     for (let x3 = 0; x3 <= 4; x3++) {
@@ -44,31 +49,50 @@ function gameOfLife () {
         }
     }
     // Update the state
-
     state = result
-    debugger;
+//    debugger;
 }
 // Use button A for the next iteration of game of life
 input.onButtonPressed(Button.A, function () {
-    priorstate = state.slice()
 //    debugger;
-    gameOfLife()
-//    debugger;
-    if ( isDead() ){
-        showERR()
-    } else {
-        if ( isSame() ){
+    if ( playerLives > 0){
+        priorstate = state.slice()
+// debugger;
+        gameOfLife()
+    // debugger;
+        if (isDead()) {
             showERR()
         } else {
-            show()
+            if (isSame()) {
+                showERR()
+            } else {
+                show()
+            }
         }
     }
 })
+function isSame () {
+    // debugger;
+    return state.every((val, index) => val === priorstate[index])
+}
 // Use button B for reseting to random initial seed state
 input.onButtonPressed(Button.B, function () {
-    reset()
-    show()
+    if (playerLives > 0) {
+        reset()
+        show()
+    }
 })
+
+input.onGesture(Gesture.Shake, function () {
+    debugger;	
+    if (playerLives < 1) {
+        playerLives = 3
+        score = 0
+        reset()
+        show()
+    } 
+})
+
 // Show the lifeChart based on the state
 function show () {
     for (let x2 = 0; x2 <= 4; x2++) {
@@ -81,22 +105,28 @@ function show () {
 }
 // blink INVERSE of the lifeChart based on the state
 function showERR () {
-    for (let x2 = 0; x2 <= 4; x2++) {
-        for (let y2 = 0; y2 <= 4; y2++) {
-            if (getState(priorstate, x2, y2)) {
-                lifeChart.setPixel(x2, y2, false );
+    for (let x22 = 0; x22 <= 4; x22++) {
+        for (let y22 = 0; y22 <= 4; y22++) {
+            if (getState(priorstate, x22, y22)) {
+                lifeChart.setPixel(x22, y22, false );
             } else {
-                lifeChart.setPixel(x2, y2, true );
+                lifeChart.setPixel(x22, y22, true );
             }
         }
     }
     lifeChart.plotImage(0);
+    playerLives -=1
+    debugger;
     basic.pause(500)
-    basic.showNumber(score)
-    basic.pause(1500)
-    score = 0
-    reset()
-    show()
+    if ( playerLives < 1) {
+        basic.showString("Score:")
+        basic.showNumber(score)
+        logo.plotImage(0)
+    } else {
+        basic.pause(1500)
+        reset()
+        show()
+    }
 }
 // Generate random initial state.
 function reset () {
@@ -106,28 +136,17 @@ function reset () {
         }
     }
 }
-// compare array
-function isDead() {
-//    debugger;
-    return state.every((val, index) => val === deadstate[index]);
-}
-
-function isSame() {
-//    debugger;
-    return state.every((val, index) => val === priorstate[index]);
-}
 /**
  * https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
  */
-
-
-let state: boolean[] = []
-let priorstate: boolean[] = []
-let deadstate: boolean[] = []
-let lifeChart: Image = null
-let count = 0
 let score = 0
 let result: boolean[] = []
+let count = 0
+let playerLives = 3
+let lifeChart: Image = null
+let deadstate: boolean[] = []
+let priorstate: boolean[] = []
+let state: boolean[] = []
 lifeChart = images.createImage(`
     . . . . .
     . . . . .
@@ -136,20 +155,27 @@ lifeChart = images.createImage(`
     . . . . .
     `)
 
+let logo = images.createImage(`
+. # # # .
+# . # . #
+# . . . #
+# . # . #
+. # # # .
+`)
+
+
 // State holds the information about pixel is live or dead
 // deadstate is ALL dead
 // priorstate is used to check if pattern is stable.   universe size apparently prevents blinkers
 // false means dead, true means live.
 state = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
 deadstate  = state.slice()
-
 function getState(arr: boolean[], x: number, y: number): boolean {
     return arr[x * 5 + y];
 }
 function setState(arr: boolean[], x: number, y: number, value: boolean): void {
     arr[x * 5 + y] = value;
 }
-
 // Initial reset & show
 reset()
 show()
